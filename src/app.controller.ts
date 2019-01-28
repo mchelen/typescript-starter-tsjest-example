fe3a6adec77dd9ba17fe5a1ba177ea8eea07e83d
Michael Chelen <mchelen@stsiinc.com>
2019-01-28 14:36:22 -0500

    tests pass

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Get, Controller } from '@nestjs/common';
 import { AppService } from './app.service';
+import { printMsg } from 'tsjest-package-example';
 
 @Controller()
 export class AppController {
@@ -7,6 +8,6 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return printMsg();
   }
 }
